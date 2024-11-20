efd89f607d7f8b1d83742d4e81f3bda3de737efa
Abdelaziz Tina <tinaabdelaziz8@gmail.com>
2024-11-20 20:49:59 +0100

    theme and fonts general keeping

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -10,17 +10,21 @@ export default async function NavBar() {
     <header className="px-5 py-3 shadow-sm font-work-sans bg-white">
       <nav className="flex justify-between items-center">
         <Link href="/">
-          <Image src="/logo.svg" alt="logo" width={144} height={30} />
+          <Image src="/logo.png" alt="logo" width={144} height={30} />
         </Link>
         <div className="flex items-center gap-4">
           {session && session.user ? (
             <>
               <Link href="/startup/create">
-                <span>Create</span>
+                <span className="font-work-sans text-black-100 font-semibold text-xl">
+                  Create
+                </span>
               </Link>
               <SignOut />
               <Link href={`/user/${session.user.id}`}>
-                <span>{session.user.name}</span>
+                <span className="font-work-sans text-black-100 font-semibold text-xl">
+                  {session.user.name}
+                </span>
               </Link>
             </>
           ) : (
